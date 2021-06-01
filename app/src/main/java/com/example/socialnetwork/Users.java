143c65dfc929918c3271953ce3bf061c5a8c742a
unknown <hoangdat1999hp@gmail.com>
2021-06-01 16:24:26 +0700

    init code

---
+++ app/src/main/java/com/example/socialnetwork/Users.java
@@ -0,0 +1,55 @@
+package com.example.socialnetwork;
+
+public class Users {
+    private String name, company, email;
+    private String uid;
+
+    public Users(String name, String company, String email, String uid) {
+        this.name = name;
+        this.company = company;
+        this.email = email;
+        this.uid = uid;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Users(){
+
+    }
+
+    public Users(String name, String company, String email) {
+        this.name = name;
+        this.company = company;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
